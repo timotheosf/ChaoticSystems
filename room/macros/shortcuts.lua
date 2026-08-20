aaa1d9d5ff1985e0a2758c93747a1afcd6bb05ad
tmfss <timoteo.fassoni@ufv.br>
2026-08-20 18:38:51 -0300

    Presentation time

--- room/macros/shortcuts.lua
+++ room/macros/shortcuts.lua
@@ -66,6 +66,80 @@ return {
     -- Como é HTML puro, usamos RawBlock. Isso garante ZERO erros com o Reveal.js!
     return pandoc.RawBlock('html', html_final)
   end,
+    ['VideoVideo'] = function(args, kwargs)
+    local desc1 = pandoc.utils.stringify(kwargs["desc1"] or "")
+    local src1 = pandoc.utils.stringify(kwargs["src1"] or "")
+    local desc2 = pandoc.utils.stringify(kwargs["desc2"] or "")
+    local src2 = pandoc.utils.stringify(kwargs["src2"] or "")
+    local is_fragment = pandoc.utils.stringify(kwargs["fragment"] or "false")
+    
+    -- Se fragment="true", adicionamos a classe do Reveal.js!
+    local frag_class = ""
+    if is_fragment == "true" then
+      frag_class = "fragment"
+    end
+
+    desc1 = desc1:gsub("%$(.-)%$", "\\(%1\\)")
+    desc2 = desc2:gsub("%$(.-)%$", "\\(%1\\)")
+    local template = [[
+<div class="%s" style="display: flex; flex-direction: row; justify-content: space-between; align-items: center; width: 100%%; height: 50vh;">
+  
+  <div style="width: 48%%; text-align: center;">
+    <video autoplay muted loop playsinline width="100%%">
+      <source src="%s" type="video/webm">
+    </video>
+    <h4 style="font-size: 1.2rem; color: #dee2e6; margin-bottom: 15px;"> %s </h4>
+  </div>
+
+  <div style="width: 48%%; text-align: center;">
+    <video autoplay muted loop playsinline width="100%%">
+      <source src="%s" type="video/webm">
+    </video>
+    <h4 style="font-size: 1.2rem; color: #dee2e6; margin-bottom: 15px;"> %s </h4>
+  </div>
+
+</div>
+]]
+
+    local html_final = string.format(template, frag_class, src1, desc1, src2, desc2)
+    return pandoc.RawBlock('html', html_final)
+  end,
+    ['VideoImage'] = function(args, kwargs)
+    local desc1 = pandoc.utils.stringify(kwargs["desc1"] or "")
+    local src1 = pandoc.utils.stringify(kwargs["src1"] or "")
+    local desc2 = pandoc.utils.stringify(kwargs["desc2"] or "")
+    local src2 = pandoc.utils.stringify(kwargs["src2"] or "")
+    local is_fragment = pandoc.utils.stringify(kwargs["fragment"] or "false")
+    
+    -- Se fragment="true", adicionamos a classe do Reveal.js!
+    local frag_class = ""
+    if is_fragment == "true" then
+      frag_class = "fragment"
+    end
+
+    desc1 = desc1:gsub("%$(.-)%$", "\\(%1\\)")
+    desc2 = desc2:gsub("%$(.-)%$", "\\(%1\\)")
+    local template = [[
+<div class="%s" style="display: flex; flex-direction: row; justify-content: space-between; align-items: center; width: 100%%; height: 50vh;">
+  
+  <div style="width: 40%%; text-align: center;">
+    <h4 style="font-size: 1.2rem; color: #dee2e6; margin-bottom: 15px;"> %s </h4>
+    <video autoplay muted loop playsinline width="100%%">
+      <source src="%s" type="video/webm">
+    </video>
+  </div>
+
+  <div style="width: 58%%; text-align: center; height: 50vh;">
+    <h4 style="font-size: 1.2rem; color: #dee2e6; margin-bottom: 15px;"> %s </h4>
+    <img src="%s" width="100%%" alt="_">
+  </div>
+
+</div>
+]]
+
+    local html_final = string.format(template, frag_class, desc1, src1, desc2, src2)
+    return pandoc.RawBlock('html', html_final)
+  end,
 
   ['InsertColumn3'] = function(args, kwargs)
     local desc1 = pandoc.utils.stringify(kwargs["desc1"] or "")
